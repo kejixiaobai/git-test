e2df81063af28d8a9faf4adfcab0a49c6768b22f
ant <259403706@qq.com>
2023-04-06 18:14:53 +0800

    second commit

--- src/main/java/com/kjxb/git/git.java
+++ src/main/java/com/kjxb/git/git.java
@@ -3,5 +3,6 @@ package com.kjxb.git;
 public class git {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
